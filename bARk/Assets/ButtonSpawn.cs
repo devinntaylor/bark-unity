d0610155d08077d09dbb9f5f15f6fffd6b1b707b
Chelsea <cej48@drexel.edu>
2021-02-17 15:01:05 -0500

    dog rotation fixed

--- bARk/Assets/ButtonSpawn.cs
+++ bARk/Assets/ButtonSpawn.cs
@@ -61,7 +61,7 @@ public class ButtonSpawn : MonoBehaviour
         if(args.added != null && placedObject == null)
         {
             ARPlane arPlane = args.added[0];
-            placedObject = Instantiate(placedPrefab, arPlane.transform.position, Quaternion.Euler(45, 0, 0));
+            placedObject = Instantiate(placedPrefab, arPlane.transform.position, Quaternion.Euler(0, -180, 0));
         }
     }
  void Update()
