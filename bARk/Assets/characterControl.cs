ff909f93f06da73d42cdb54ada7fc51756788e05
Chelsea <cej48@drexel.edu>
2021-03-10 15:59:05 -0500

    tried tutorial

---
+++ bARk/Assets/characterControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class characterControl : MonoBehaviour
+{
+    private Animator Anim; 
+    public int State = 0;
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+    }
+
+  
+    public void animTrig()
+    {
+        if (State == 0)
+        {
+            Anim.SetTrigger("P");
+            State = 1;
+            return;
+        }
+         if (State == 1)
+        {
+            Anim.SetTrigger("D");
+            State = 2;
+            return;
+        }
+         if (State == 2)
+        {
+            Anim.SetTrigger("S");
+            State = 0;
+            return;
+        }
+    }
+}
---
+++ bARk/Temp/il2cppOutput/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,71 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void PlaceOnPlane::Awake()
+extern void PlaceOnPlane_Awake_mE85A120DA51619398754C88AE48C78A329B5D265 (void);
+// 0x00000002 System.Void PlaceOnPlane::PlaneChanged(UnityEngine.XR.ARFoundation.ARPlanesChangedEventArgs)
+extern void PlaceOnPlane_PlaneChanged_mEAABA9A8FEC2BAEC0DFD676B864244C91F4AC9BF (void);
+// 0x00000003 System.Void PlaceOnPlane::.ctor()
+extern void PlaceOnPlane__ctor_mECF29DC898119BD47C75A1D3845F362668A10AB0 (void);
+// 0x00000004 System.Void PlaceOnPlane::.cctor()
+extern void PlaceOnPlane__cctor_m99BEA781E46A1FB9FAF672FAF883E92F9B1D8E4A (void);
+// 0x00000005 System.Void characterControl::Start()
+extern void characterControl_Start_mC9545E349F89529BC1544462B1885D063E0FD330 (void);
+// 0x00000006 System.Void characterControl::animTrig()
+extern void characterControl_animTrig_m042221B5A7CA07AE84FDBFC8E8637D5AA5B55014 (void);
+// 0x00000007 System.Void characterControl::.ctor()
+extern void characterControl__ctor_mC12F836A2842AA68306F6F58440CAC4A4F7D79AE (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	PlaceOnPlane_Awake_mE85A120DA51619398754C88AE48C78A329B5D265,
+	PlaceOnPlane_PlaneChanged_mEAABA9A8FEC2BAEC0DFD676B864244C91F4AC9BF,
+	PlaceOnPlane__ctor_mECF29DC898119BD47C75A1D3845F362668A10AB0,
+	PlaceOnPlane__cctor_m99BEA781E46A1FB9FAF672FAF883E92F9B1D8E4A,
+	characterControl_Start_mC9545E349F89529BC1544462B1885D063E0FD330,
+	characterControl_animTrig_m042221B5A7CA07AE84FDBFC8E8637D5AA5B55014,
+	characterControl__ctor_mC12F836A2842AA68306F6F58440CAC4A4F7D79AE,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	23,
+	1653,
+	23,
+	3,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
